6e6bdba8a8716f0180a9e24de5801b70a6ebda4e
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-09-19 22:58:49 +0100

    Update

---
+++ src/Testing/TestingServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BristolSU\Support\Testing;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class TestingServiceProvider extends ServiceProvider
+{
+    
+}
