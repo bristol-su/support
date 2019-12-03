9eb45d36eaa41e62f4e9187c5bf4357b30a3e1dd
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-12-03 22:12:58 +0000

    Automatically pass in the user, group and role to an actiit activity evaluator

---
+++ src/Filters/Filters/Group/GroupNameIs.php
@@ -0,0 +1,84 @@
+<?php
+
+
+namespace BristolSU\Support\Filters\Filters\User;
+
+
+use BristolSU\Support\DataPlatform\Contracts\Repositories\User as DataUserRepository;
+use BristolSU\Support\Filters\Contracts\Filters\UserFilter;
+use \BristolSU\Support\Control\Contracts\Repositories\User as UserRepository;
+
+/**
+ * Class UserEmailIs
+ * @package BristolSU\Support\Filters\Filters
+ */
+class UserEmailIs extends UserFilter
+{
+
+    /**
+     * @var DataUserRepository
+     */
+    private $dataUserRepository;
+
+    /**
+     * UserEmailIs constructor.
+     * @param UserRepository $userRepository
+     * @param DataUserRepository $dataUserRepository
+     */
+    public function __construct(DataUserRepository $dataUserRepository)
+    {
+        $this->dataUserRepository = $dataUserRepository;
+    }
+
+    /**
+     * Get possible options as an array
+     *
+     * @return array
+     */
+    public function options(): array
+    {
+        return [
+            'email' => ''
+        ];
+    }
+
+    /**
+     * Test if the filter passes
+     *
+     * @param string $settings Key of the chosen option
+     *
+     * @return bool
+     */
+    public function evaluate($settings): bool
+    {
+        try {
+            $user = $this->dataUserRepository->getById($this->user()->dataPlatformId());
+        } catch (\Exception $e) {
+            return false;
+        }        return $user->email === $settings['email'];
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function name()
+    {
+        return 'User has Email';
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function description()
+    {
+        return 'User has a given email address';
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function alias()
+    {
+        return 'user_email_is';
+    }
+}
