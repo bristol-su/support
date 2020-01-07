d7058461a1a312946bb6b0f55fefd083be507a7c
Toby Twigger <tobytwigger1@gmail.com>
2020-01-07 15:00:59 +0000

    Update helpers docs

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -1,15 +1,17 @@
 <?php
 
 use BristolSU\Support\ModuleInstance\ModuleInstance;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\View;
 
 if(!function_exists('settings')) {
     /**
-     * @param null $key
-     * @param null $default
-     * @return |null |null |null
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * Get the value of a setting for a module
+     *
+     * @param string $key Key of the setting to retrieve
+     * @param mixed $default Default value to return if no setting found. Defaults to null
+     * @return mixed
+     * @throws BindingResolutionException If the module instance cannot be found
      */
     function settings($key = null, $default = null)
     {
@@ -30,6 +32,12 @@ if(!function_exists('settings')) {
 }
 
 if(!function_exists('alias')) {
+    /**
+     * Get the alias of the currently injected module instance
+     * 
+     * @return string Alias of the module instance
+     * @throws BindingResolutionException If the module instance cannot be found
+     */
     function alias() {
         $moduleInstance = app()->make(ModuleInstance::class);
         if($moduleInstance->exists) {
