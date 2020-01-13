4092d6838654407481910da4989bdf8555a23ac2
Toby Twigger <tobytwigger1@gmail.com>
2020-01-13 13:29:17 +0000

    Conditionally add header login buttons if route exists

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
             <a class="navbar-brand">
                 Portal v3
             </a>
-            
+
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="{{ __('Toggle navigation') }}">
@@ -15,22 +15,24 @@
             <!-- Right side of NavBar -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
-                    @guest
-                    <!-- Login and register links for non logged in users -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @else
+                @guest
+                    @if(\Illuminate\Support\Facades\Route::has('login'))
+                        <!-- Login and register links for non logged in users -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                    @endif
+                @else
                     <!-- Account Management -->
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 {{ $authentication->getUser()->forename }} <span class="caret"></span>
                             </a>
-    
+
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="/">Home</a>
                                 <a class="dropdown-item" href="#"
