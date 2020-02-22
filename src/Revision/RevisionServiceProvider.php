817808c14962cedc29775014399be863c6e99fc7
Toby Twigger <tt15951@my.bristol.ac.uk>
2020-02-22 11:38:00 +0000

    Correctly merge config and migrations

--- src/Revision/RevisionServiceProvider.php
+++ src/Revision/RevisionServiceProvider.php
@@ -4,16 +4,45 @@ namespace BristolSU\Support\Revision;
 
 use Venturecraft\Revisionable\RevisionableServiceProvider as BaseServiceProvider;
 
+/**
+ * Override the default revision service provider
+ */
 class RevisionServiceProvider extends BaseServiceProvider
 {
 
-    protected $baseDirectory = __DIR__ . '/../../vendor/venturecraft/revisionable';
-    
-    public function register()
+    /**
+     * Merge the config and migrations
+     * 
+     * The VentureCraft package has not merged config or loaded migrations, therefore the SDK is unable to use them
+     * by default. To get around this, we merge it here, using the base directory from the 'publishes' array.
+     */
+    public function boot()
     {
-        parent::register();
-        $this->mergeConfigFrom($this->baseDirectory . '/src/config/revisionable.php', 'revisionable');
-        $this->loadMigrationsFrom($this->baseDirectory . '/src/migrations');
+        parent::boot();
+        $this->mergeConfig();
+        $this->loadMigrations();
     }
-    
+
+    protected function mergeConfig()
+    {
+        if(array_key_exists(static::class, static::$publishes)) {
+            foreach(static::$publishes[static::class] as $key => $value) {
+                if(strpos($key, 'config/revisionable.php') !== false && file_exists($key)) {
+                    $this->mergeConfigFrom($key, 'revisionable');
+                }
+            }
+        }
+    }
+
+    protected function loadMigrations()
+    {
+        if(array_key_exists(static::class, static::$publishes)) {
+            foreach(static::$publishes[static::class] as $key => $value) {
+                if(strpos($key, 'migrations') !== false && is_dir($key)) {
+                    $this->loadMigrationsFrom($key);
+                }
+            }
+        }
+    }
+
 }
