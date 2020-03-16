29020b44805b5ef7900ff8dde944a44c7e67d5f5
Toby Twigger <tobytwigger1@gmail.com>
2020-03-16 12:36:59 +0000

    Update changelog and docs

--- docs/search/all_15.js
+++ docs/search/all_15.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['websessionauthentication',['WebSessionAuthentication',['../classBristolSU_1_1Support_1_1Authentication_1_1WebSessionAuthentication.html',1,'BristolSU::Support::Authentication']]],
-  ['websessionauthentication_2ephp',['WebSessionAuthentication.php',['../WebSessionAuthentication_8php.html',1,'']]],
+  ['webrequestactivityinstanceresolver',['WebRequestActivityInstanceResolver',['../classBristolSU_1_1Support_1_1ActivityInstance_1_1WebRequestActivityInstanceResolver.html',1,'BristolSU::Support::ActivityInstance']]],
+  ['webrequestactivityinstanceresolver_2ephp',['WebRequestActivityInstanceResolver.php',['../WebRequestActivityInstanceResolver_8php.html',1,'']]],
+  ['webrequestauthentication',['WebRequestAuthentication',['../classBristolSU_1_1Support_1_1Authentication_1_1WebRequestAuthentication.html',1,'BristolSU::Support::Authentication']]],
+  ['webrequestauthentication_2ephp',['WebRequestAuthentication.php',['../WebRequestAuthentication_8php.html',1,'']]],
   ['withaccesstologicgroupwithresource',['withAccessToLogicGroupWithResource',['../classBristolSU_1_1Support_1_1Logic_1_1Audience_1_1AudienceMemberFactory.html#ac5bf1b5c40f09555bf26d0445d67b501',1,'BristolSU\Support\Logic\Audience\AudienceMemberFactory\withAccessToLogicGroupWithResource()'],['../interfaceBristolSU_1_1Support_1_1Logic_1_1Contracts_1_1Audience_1_1AudienceMemberFactory.html#a8e66b1dde4ff1aca6c72135c12b3578c',1,'BristolSU\Support\Logic\Contracts\Audience\AudienceMemberFactory\withAccessToLogicGroupWithResource()']]],
   ['withaccesstoresource',['withAccessToResource',['../classBristolSU_1_1Support_1_1Logic_1_1Audience_1_1AudienceMemberFactory.html#a70079780995034af7eddee9ccb61741b',1,'BristolSU\Support\Logic\Audience\AudienceMemberFactory\withAccessToResource()'],['../interfaceBristolSU_1_1Support_1_1Logic_1_1Contracts_1_1Audience_1_1AudienceMemberFactory.html#ae26649d898d8ecc3ecb13288fdf49bbb',1,'BristolSU\Support\Logic\Contracts\Audience\AudienceMemberFactory\withAccessToResource()']]]
 ];
--- docs/search/files_7.js
+++ docs/search/files_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['incorrectlogin_2ephp',['IncorrectLogin.php',['../IncorrectLogin_8php.html',1,'']]],
   ['injectactivity_2ephp',['InjectActivity.php',['../InjectActivity_8php.html',1,'']]],
   ['injectactivityinstance_2ephp',['InjectActivityInstance.php',['../InjectActivityInstance_8php.html',1,'']]],
   ['injectjavascriptvariables_2ephp',['InjectJavascriptVariables.php',['../InjectJavascriptVariables_8php.html',1,'']]],
