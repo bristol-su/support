eeea4413c21916df7d23624d0e83b695a450574f
Toby Twigger <tobytwigger1@gmail.com>
2020-12-12 23:32:23 +0000

    Move laravel auth scaffolding to sdk

--- src/Authentication/AuthenticationServiceProvider.php
+++ src/Authentication/AuthenticationServiceProvider.php
@@ -5,6 +5,7 @@ namespace BristolSU\Support\Authentication;
 use BristolSU\Support\Authentication\AuthQuery\Generator;
 use BristolSU\Support\Authentication\Contracts\ResourceIdGenerator;
 use Illuminate\Routing\UrlGenerator;
+use BristolSU\Support\Authentication\Contracts\Authentication;
 use Illuminate\Support\ServiceProvider;
 
 class AuthenticationServiceProvider extends ServiceProvider
@@ -13,6 +14,12 @@ class AuthenticationServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(ResourceIdGenerator::class, AuthenticationResourceIdGenerator::class);
+   
+        $this->app->rebinding('request', function ($app, $request) {
+            $request->setUserResolver(function () use ($app) {
+                return $app->make(Authentication::class)->getUser();
+            });
+        });
     }
 
     public function boot()
@@ -22,6 +29,10 @@ class AuthenticationServiceProvider extends ServiceProvider
         });
         UrlGenerator::macro('getAuthQueryString', function() {
             return app(Generator::class)->getAuthCredentials()->toString();
+        });
+        
+        $this->app['auth']->resolveUsersUsing(function() {
+            return app()->make(Authentication::class)->getUser();
         });
     }
 
