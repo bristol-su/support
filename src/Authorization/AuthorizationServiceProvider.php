d5d5ec0b35763dc9bfc24c10a3ebf36060c92885
Toby Twigger <tobytwigger1@gmail.com>
2020-12-31 01:17:45 +0000

    Change activity enabled to always be checked

--- src/Authorization/AuthorizationServiceProvider.php
+++ src/Authorization/AuthorizationServiceProvider.php
@@ -23,7 +23,7 @@ class AuthorizationServiceProvider extends ServiceProvider
     {
         $this->app['router']->pushMiddlewareToGroup('participant', CheckLoggedIntoActivityForType::class);
         $this->app['router']->pushMiddlewareToGroup('participant', CheckActivityFor::class);
-        $this->app['router']->pushMiddlewareToGroup('participant', CheckActivityEnabled::class);
+        $this->app['router']->pushMiddlewareToGroup('activity', CheckActivityEnabled::class);
         $this->app['router']->pushMiddlewareToGroup('module', CheckModuleInstanceEnabled::class);
         $this->app['router']->pushMiddlewareToGroup('module', CheckModuleInstanceActive::class);
         $this->app['router']->pushMiddlewareToGroup('administrator', CheckAdminActivityFor::class);
