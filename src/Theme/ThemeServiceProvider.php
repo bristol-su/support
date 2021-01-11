b5b4da2fd15bfd8c3e253a8df952d9c8f6607ad2
Toby Twigger <tobytwigger1@gmail.com>
2021-01-11 02:09:48 +0000

    Load the needed themes

---
+++ src/Theme/ThemeServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BristolSU\Support\Theme;
+
+use BristolSU\Support\Settings\Concerns\RegistersSettings;
+use BristolSU\Support\Theme\Settings\AppearanceCategory;
+use BristolSU\Support\Theme\Settings\ChosenTheme;
+use BristolSU\Support\Theme\Settings\ThemeGroup;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\ServiceProvider;
+use Twigger\Blade\Foundation\ThemeStore;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    use RegistersSettings;
+
+    public function register()
+    {
+        // No registration
+    }
+
+    public function boot()
+    {
+        $this->registerSettings()
+            ->category(new AppearanceCategory())
+            ->group(new ThemeGroup())
+            ->registerSetting(new ChosenTheme(app(ThemeStore::class)));
+
+        try {
+            $theme = ChosenTheme::getValue();
+            \Twigger\Blade\ThemeServiceProvider::useTheme($theme);
+        } catch (QueryException $e) {
+            // Theme couldn't be loaded as settings table hasn't yet been migrated.
+        }
+
+        $this->app['config']->set('themes.tag-prefix', 'su');
+    }
+
+}
