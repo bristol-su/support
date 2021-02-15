aefd3039ff9f11b8ff80dfbeba7862dde8ebe1fb
Toby Twigger <tobytwigger1@gmail.com>
2021-02-15 22:43:25 +0000

    Fix data providers

--- tests/Progress/ProgressUpdateRepositoryTest.php
+++ tests/Progress/ProgressUpdateRepositoryTest.php
@@ -51,125 +51,111 @@ class ProgressUpdateRepositoryTest extends TestCase
 
     /**
      * @test
-     * @dataProvider progressChangeProvider
      */
-    public function saving_progress_updates_a_hash_when_a_progress_is_changed(\Closure $getData)
+    public function saving_progress_updates_a_hash_when_a_progress_is_changed()
     {
-        [$firstProgress, $changedProgress] = $getData();
+        foreach ($this->progressChangeProvider() as $case => $arguments) {
+            [$firstProgress, $changedProgress] = $arguments[0];
 
-        $this->repository->saveChanges($this->activityInstance->id, 'some_caller', $firstProgress);
+            $this->repository->saveChanges($this->activityInstance->id, 'some_caller', $firstProgress);
 
-        $this->assertTrue(
-            $this->repository->hasChanged($this->activityInstance->id, 'some_caller', $changedProgress)
-        );
+            $this->assertTrue(
+                $this->repository->hasChanged($this->activityInstance->id, 'some_caller', $changedProgress),
+                sprintf('Test case %s expected hasChanged to be true', $case)
+            );
+        }
     }
 
     public function progressChangeProvider(): array
     {
         return [
             'Return two set of Progresses' => [
-                function () {
-                    return [
-                        $this->createProgress(),
-                        $this->createProgress(function (Progress $progress): Progress {
-                            $progress->setActivityId(factory(Activity::class)->create()->id);
-                            $progress->setActivityInstanceId(factory(ActivityInstance::class)->create()->id);
+                [
+                    $this->createProgress(),
+                    $this->createProgress(function (Progress $progress): Progress {
+                        $progress->setActivityId(factory(Activity::class)->create()->id);
+                        $progress->setActivityInstanceId(factory(ActivityInstance::class)->create()->id);
 
-                            return $progress;
-                        })
-                    ];
-                }
+                        return $progress;
+                    })
+                ]
             ],
             'Return two set of Activities with different percentages' => [
-                function () {
-                    return [
-                        $this->createProgress(),
-                        $this->createProgress(function (Progress $progress): Progress {
-                            $progress->setActivityId(factory(Activity::class)->create()->id);
-                            $progress->setPercentage(50);
+                [
+                    $this->createProgress(),
+                    $this->createProgress(function (Progress $progress): Progress {
+                        $progress->setActivityId(factory(Activity::class)->create()->id);
+                        $progress->setPercentage(50);
 
-                            return $progress;
-                        })
-                    ];
-                }
+                        return $progress;
+                    })
+                ]
             ],
             'Return two set of Activities with different complete statuses' => [
-                function () {
-                    return [
-                        $this->createProgress(),
-                        $this->createProgress(function (Progress $progress): Progress {
-                            $progress->setActivityId(factory(Activity::class)->create()->id);
-                            $progress->isComplete(false);
+                [
+                    $this->createProgress(),
+                    $this->createProgress(function (Progress $progress): Progress {
+                        $progress->setActivityId(factory(Activity::class)->create()->id);
+                        $progress->setComplete(false);
 
-                            return $progress;
-                        })
-                    ];
-                }
+                        return $progress;
+                    })
+                ]
             ],
             'Return two set of Progresses with Module Mandatory changed' => [
-                function () {
-                    return [
-                        $this->createProgress(),
-                        $this->createProgress(function (Progress $progress): Progress {
-                            $progress->setActivityId(factory(Activity::class)->create()->id);
-                            $progress->getModules()[0]->setMandatory(false);
+                [
+                    $this->createProgress(),
+                    $this->createProgress(function (Progress $progress): Progress {
+                        $progress->setActivityId(factory(Activity::class)->create()->id);
+                        $progress->getModules()[0]->setMandatory(false);
 
-                            return $progress;
-                        })
-                    ];
-                }
+                        return $progress;
+                    })
+                ]
             ],
             'Return two set of Progresses with Module Complete changed' => [
-                function () {
-                    return [
-                        $this->createProgress(),
-                        $this->createProgress(function (Progress $progress): Progress {
-                            $progress->setActivityId(factory(Activity::class)->create()->id);
-                            $progress->getModules()[0]->setComplete(false);
+                [
+                    $this->createProgress(),
+                    $this->createProgress(function (Progress $progress): Progress {
+                        $progress->setActivityId(factory(Activity::class)->create()->id);
+                        $progress->getModules()[0]->setComplete(false);
 
-                            return $progress;
-                        })
-                    ];
-                }
+                        return $progress;
+                    })
+                ]
             ],
             'Return two set of Progresses with Module Percentage changed' => [
-                function () {
-                    return [
-                        $this->createProgress(),
-                        $this->createProgress(function (Progress $progress): Progress {
-                            $progress->setActivityId(factory(Activity::class)->create()->id);
-                            $progress->getModules()[0]->setPercentage(10);
+                [
+                    $this->createProgress(),
+                    $this->createProgress(function (Progress $progress): Progress {
+                        $progress->setActivityId(factory(Activity::class)->create()->id);
+                        $progress->getModules()[0]->setPercentage(10);
 
-                            return $progress;
-                        })
-                    ];
-                }
+                        return $progress;
+                    })
+                ]
             ],
             'Return two set of Progresses with Module Active changed' => [
-                function () {
-                    return [
-                        $this->createProgress(),
-                        $this->createProgress(function (Progress $progress): Progress {
-                            $progress->setActivityId(factory(Activity::class)->create()->id);
-                            $progress->getModules()[0]->setActive(false);
+                [
+                    $this->createProgress(),
+                    $this->createProgress(function (Progress $progress): Progress {
+                        $progress->setActivityId(factory(Activity::class)->create()->id);
+                        $progress->getModules()[0]->setActive(false);
 
-                            return $progress;
-                        })
-                    ];
-                }
+                        return $progress;
+                    })
+                ]
             ],
             'Return two set of Progresses with Module Visibility changed' => [
-                function () {
-                    return [
-                        $this->createProgress(),
-                        $this->createProgress(function (Progress $progress): Progress {
-                            $progress->setActivityId(factory(Activity::class)->create()->id);
-                            $progress->getModules()[0]->setVisible(false);
+                [
+                    $this->createProgress(),
+                    $this->createProgress(function (Progress $progress): Progress {
+                        $progress->setActivityId(factory(Activity::class)->create()->id);
+                        $progress->getModules()[0]->setVisible(false);
 
-                            return $progress;
-                        })
-                    ];
-                }
+                        return $progress;
+                    })
+                ]
             ]
         ];
     }
@@ -207,7 +193,7 @@ class ProgressUpdateRepositoryTest extends TestCase
         $progress->pushModule($moduleProgress2);
 
         if ($callback === null) {
-            $callback = fn (Progress $progress): Progress => $progress;
+            $callback = fn(Progress $progress): Progress => $progress;
         }
 
         return $callback($progress);
@@ -225,10 +211,7 @@ class ProgressUpdateRepositoryTest extends TestCase
         $this->assertFalse($this->repository->hasChanged($this->activityInstance->id, 'caller', $progress));
     }
 
-    /**
-     *
-     * @test
-     */
+    /** @test */
     public function has_changed_returns_true_if_progress_has_changed_since_last_save()
     {
         $progress = $this->createProgress();
@@ -242,14 +225,20 @@ class ProgressUpdateRepositoryTest extends TestCase
         $this->assertTrue($this->repository->hasChanged($this->activityInstance->id, 'caller', $progress));
     }
 
-    /**
-     * @dataProvider progressChangeProvider
-     * @test
-     */
-    public function has_changed_returns_true_if_the_progress_is_not_saved(\Closure $getData)
+    /** @test */
+    public function has_changed_returns_true_if_the_progress_is_not_saved()
     {
-        [$progress] = $getData();
+        foreach ($this->progressChangeProvider() as $case => $arguments) {
+            [$firstProgress, $changedProgress] = $arguments[0];
 
-        $this->assertTrue($this->repository->hasChanged($this->activity->id, 'caller', $progress));
+            $this->assertTrue(
+                $this->repository->hasChanged(
+                    $this->activity->id,
+                    'caller',
+                    $firstProgress
+                ),
+                sprintf('Test case %s expected hasChanged to be true', $case)
+            );
+        }
     }
 }
