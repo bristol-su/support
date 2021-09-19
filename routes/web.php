10269d61da00d21d3a20ba70a560a4643ce439c6
Toby Twigger <tobytwigger1@gmail.com>
2021-09-19 22:39:26 +0100

    Fix with route names

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['participant, administrator, module, activity, participant-activity, participant-module'])->group(function() {
-    Route::get('/{a_or_p}/{activity_slug}/{module_instance_slug}', [\BristolSU\Support\Http\Controllers\ModuleInstanceRedirectController::class, 'index'])
-        ->where('a_or_p', '(a | p)')
-        ->name('module');
+Route::middleware(['module', 'activity', 'participant-activity', 'participant-module'])->group(function() {
+    Route::middleware('participant')->get(
+        '/p/{activity_slug}/{module_instance_slug}',
+        [\BristolSU\Support\Http\Controllers\ModuleInstanceRedirectController::class, 'index']
+    )->name('module.participant');
+    Route::middleware('administrator')->get(
+        '/a/{activity_slug}/{module_instance_slug}',
+        [\BristolSU\Support\Http\Controllers\ModuleInstanceRedirectController::class, 'index']
+    )->name('module.admin');
 });
-
-
