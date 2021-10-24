c9a2cc9b8563af1cc7bfdc5714b458eb0b289c88
Toby Twigger <tobytwigger1@gmail.com>
2021-10-24 22:00:38 +0100

    Remove old configuration related to filter caching

--- config/config.php
+++ config/config.php
@@ -12,12 +12,6 @@ return [
             'limit' => env('REVISION_CLEANUP_LIMIT', 10000)
         ]
     ],
-    'caching' => [
-        'filters' => [
-            'enabled' => env('FILTER_CACHING_ENABLED', true),
-            'duration' => env('FILTER_CACHING_DURATION', 900)
-        ]
-    ],
     'progress' => [
         'default' => 'database',
         'export' => [
