9bf3697515a9bc0b49750b025524b8c42267bf6c
Toby Twigger <tobytwigger1@gmail.com>
2022-05-23 13:21:38 +0100

    Add indexes for user, group and role iD

---
+++ database/migrations/2022_05_23_000808_add_single_indexes_to_logic_results_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSingleIndexesToLogicResultsAgainTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     */
+    public function up()
+    {
+        Schema::table('logic_results', function (Blueprint $table) {
+            $table->index(['user_id'], 'log_res_user_index');
+            $table->index(['group_id'], 'log_res_grp_index');
+            $table->index(['role_id'], 'log_res_role_index');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     */
+    public function down()
+    {
+        Schema::table('logic_results', function(Blueprint $table) {
+            $table->dropIndex('log_res_user_index');
+            $table->dropIndex('log_res_grp_index');
+            $table->dropIndex('log_res_role_index');
+        });
+    }
+}
