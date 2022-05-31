75c12b31f8795018224712137dba66f198f60df5
Toby Twigger <tobytwigger1@gmail.com>
2022-05-31 13:49:01 +0100

    Add logging to progress jobs

--- src/Progress/Jobs/UpdateProgressForGivenActivityInstances.php
+++ src/Progress/Jobs/UpdateProgressForGivenActivityInstances.php
@@ -40,13 +40,17 @@ class UpdateProgressForGivenActivityInstances implements ShouldQueue
 
     public function handle(Snapshot $snapshot)
     {
+        Log::info('Starting to process activity instances');
         $progresses = [];
         foreach ($this->activityInstances as $activityInstance) {
+            Log::info('Processing activity instance for #' . $activityInstance->id);
             $progresses[] = $snapshot->ofUpdateToActivityInstance($activityInstance, $this->driver);
         }
 
         $filteredProgresses = array_values(array_filter($progresses));
 
+        Log::info('Ready to export progress!');
+        
         if ($filteredProgresses) {
             ProgressExport::driver($this->driver)->saveMany($filteredProgresses);
         }
